8341716ce5de5bb45fb9d604e0836da8eae58bc6
RomaSamkov <Romansamkov.rs@gmail.com>
2023-05-22 15:08:06 +0300

    Update page.tsx

--- app/[searchTerm]/page.tsx
+++ app/[searchTerm]/page.tsx
@@ -1,4 +1,5 @@
 import getWikiResults from "@/lib/getWikiResults";
+import Item from "./components/Item";
 
 type Props = {
   params: {
@@ -32,8 +33,8 @@ export default async function SearchResults({ params: { searchTerm } }: Props) {
     <main className="bg-slate-200 mx-auto max-w-lg py-1 min-h-screen">
       {results ? (
         Object.values(results).map((result) => {
-          // return <Item key={result.pageid} result={result} />;
-          return <p key={result.pageid}>{JSON.stringify(result)}</p>;
+          return <Item key={result.pageid} result={result} />;
+          // return <p key={result.pageid}>{JSON.stringify(result)}</p>;
         })
       ) : (
         <h2 className="p-2 text-xl">{`${searchTerm} Not Found`}</h2>
